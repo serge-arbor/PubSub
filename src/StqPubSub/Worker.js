e34ff58ad966dd87c944e6c61ba6fd076f6149c2
Serge Arbor <serge.arbor@gmail.com>
2023-05-26 19:06:04 +0200

    refactored

--- src/StqPubSub/Worker.js
+++ src/StqPubSub/Worker.js
@@ -5,21 +5,49 @@ class MessageWorker {
   }
 
   async processMessage(message, formattedSubscription, processorFunction) {
+    this.validateProcessorFunction(processorFunction);
+
+    const processingPromise = processorFunction(message);
+    this.extendAckDeadlineUntilResolved(processingPromise, message, formattedSubscription);
+  
+    await processingPromise;
+    await this.acknowledgeMessage(message, formattedSubscription);
+  }
+  
+  validateProcessorFunction(processorFunction) {
     if (typeof processorFunction !== 'function') {
       console.log(processorFunction)
       throw new TypeError("processorFunction must be a function");
     }
-    
-    const processingPromise = processorFunction(message);
+  }
 
-    const deadlineExtensionPromise = this.extendAckDeadlineUntilResolved(
-      processingPromise, 
-      message, 
-      formattedSubscription
-    );
+  async extendAckDeadlineUntilResolved(processingPromise, message, formattedSubscription) {
+    while (await this.isProcessingIncomplete(processingPromise)) {
+      await this.modifyAckDeadline(message, formattedSubscription);
+    }
+  }
 
-    await Promise.all([processingPromise, deadlineExtensionPromise]);
+  async isProcessingIncomplete(processingPromise) {
+    const winner = await Promise.race([
+      processingPromise,
+      new Promise((resolve) => setTimeout(resolve, 10000, 'timeout')),
+    ]);
 
+    return winner === 'timeout';
+  }
+
+  async modifyAckDeadline(message, formattedSubscription) {
+    const modifyAckDeadlineRequest = {
+      subscription: formattedSubscription,
+      ackIds: [message.ackId],
+      ackDeadlineSeconds: this.ackDeadlineSeconds,
+    };
+
+    await this.pubSubClient.modifyAckDeadline(modifyAckDeadlineRequest);
+    console.log(`Reset ack deadline for "${message.message.data}" for ${this.ackDeadlineSeconds}s.`);
+  }
+
+  async acknowledgeMessage(message, formattedSubscription) {
     const ackRequest = {
       subscription: formattedSubscription,
       ackIds: [message.ackId],
@@ -27,28 +55,6 @@ class MessageWorker {
 
     await this.pubSubClient.acknowledge(ackRequest);
     console.log(`Acknowledged: "${message.message.data}"`);
-  }
-
-  async extendAckDeadlineUntilResolved(processingPromise, message, formattedSubscription) {
-    while (true) {
-      const winner = await Promise.race([
-        processingPromise,
-        new Promise((resolve) => setTimeout(resolve, 10000, 'timeout')),
-      ]);
-
-      if (winner !== 'timeout') break;
-
-      const modifyAckDeadlineRequest = {
-        subscription: formattedSubscription,
-        ackIds: [message.ackId],
-        ackDeadlineSeconds: this.ackDeadlineSeconds,
-      };
-
-      await this.pubSubClient.modifyAckDeadline(modifyAckDeadlineRequest);
-      console.log(
-        `Reset ack deadline for "${message.message.data}" for ${this.ackDeadlineSeconds}s.`
-      );
-    }
   }
 }
 
