a3f6b682fc99eb0c2792ee7a06c5d7fb3ff3289c
Serge Arbor <serge.arbor@gmail.com>
2023-06-05 11:48:06 +0200

    upd

--- src/init_client.js
+++ src/init_client.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const StqPubSubApi = require('./StqPubSub/Api');
 
-const client = new StqPubSubApi(process.env.GCP_KEY_PATH);
+const client = new StqPubSubApi(process.env.GCP_KEY_PATH || '~/key.json');
 
 module.exports = client;
